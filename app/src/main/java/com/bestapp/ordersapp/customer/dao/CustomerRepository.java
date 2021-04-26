7a72d985ec387ab075d0a7c9ae8baf5a1cae5b8a
Bantea Carmen <bantea.carmen93@gmail.com>
2021-04-26 13:31:24 +0300

    findCustomerByEmail query added in customer Repository

--- app/src/main/java/com/bestapp/ordersapp/customer/dao/CustomerRepository.java
+++ app/src/main/java/com/bestapp/ordersapp/customer/dao/CustomerRepository.java
@@ -3,8 +3,14 @@ package com.bestapp.ordersapp.customer.dao;
 
 import com.bestapp.ordersapp.customer.model.persistance.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+
+    @Query(value = "select c.* from orders.customers c join orders.users u on u.id = c.user_id where u.email =:email", nativeQuery = true)
+    public Optional<CustomerEntity> findCustomerByEmail(String email);
 }
